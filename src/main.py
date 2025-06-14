522e1c152ede3ffff636071201cf28e12126396a
quixxy <quixxy7@gmail.com>
2025-06-14 19:11:01 +0700

    added update and mark task methods

--- src/main.py
+++ src/main.py
@@ -3,8 +3,17 @@ from tasks import Task, TaskManager
 def main():
     tm = TaskManager()
     tm.load_tasks()
-
+    tm.add_task(Task("zbc", "aheuna"))
     tm.print_tasks()
+    taskID = int(input("Enter ID: "))
+    tm.update_task(taskID)
+    taskID = int(input("Enter ID: "))
+    tm.mark_task(taskID)
+    tm.print_tasks()
+
+    ans = input("Do you want to save changes?: (Y/n)")
+    if "n" not in ans:
+        tm.save_tasks()
 
 if __name__ == "__main__":
     main()
--- src/tasks/taskmanager.py
+++ src/tasks/taskmanager.py
@@ -1,10 +1,15 @@
-from tasks.task import Task
+from tasks.task import Task, TaskStatus
 from tasks.storage import Storage
+from datetime import date
 
 class TaskManager:
     def __init__(self):
         self.task_id: int = 1
         self.tasks: dict[int, Task] = {}
+
+    def clear(self) -> None:
+        self.task_id = 1
+        self.tasks = {}
 
     def add_task(self, task: Task) -> None:
         self.tasks[self.task_id] = task
@@ -14,7 +19,34 @@ class TaskManager:
         self.tasks.pop(task_id)
 
     def update_task(self, task_id: int) -> None:
-        pass
+        updated = False
+
+        print("| Leave the field blank if you want to leave it unchanged |")
+        u_name = input("Enter new name: ")
+        u_description = input("Enter new description: ")
+
+        if u_name:
+            self.tasks[task_id].name = u_name
+            updated = True
+        if u_description:
+            self.tasks[task_id].description = u_description
+            updated =True
+
+        if updated:
+            self.tasks[task_id].updatedAt = date.today()
+
+    def mark_task(self, task_id: int) -> None:
+        new_status = int(input("Enter new status (1 - todo, 2 - in-progress, 3 - done): "))
+        match new_status:
+            case 1:
+                self.tasks[task_id].status = TaskStatus.TODO
+            case 2:
+                self.tasks[task_id].status = TaskStatus.IN_PROGRESS
+            case 3:
+                self.tasks[task_id].status = TaskStatus.DONE
+            case _:
+                print("Wrong status")
+
 
     def print_tasks(self) -> None:
         if not self.tasks:
